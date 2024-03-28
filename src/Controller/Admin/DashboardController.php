c97c23084af406df768fb12fb60778fb36e867a8
AdamRse <bakaarion3@gmail.com>
2024-03-28 10:55:48 +0100

    init

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -17,9 +17,11 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class DashboardController extends AbstractDashboardController
 {
+    #[IsGranted("ROLE_ADMIN")]
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
